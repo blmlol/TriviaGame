0bb6387a321ea631383a9ea4608e79a745141693
Davey Wong <notwdjw@gmail.com>
2019-11-08 13:55:42 -0800

    Added 10 questions as an array of objects each with multiple choice answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,76 @@
+//For the trivia questions, we will want to use an array of objects that have a question and ABCD answers
+// This way we can loop through the array and display the question and all of the answers at once
+//The theme of questions will come from history
+
+var qanda = [
+    {
+        question: "Which US president served the shortest term in office?",
+        A: "William Henry Harrison",
+        B: "James A. Garfield",
+        C: "Zachary Taylor",
+        D: "Gerald Ford"
+    },
+    {
+        question: "In what year was the Louisiana Purchase made?",
+        A: "1784",
+        B: "1909",
+        C: "1803",
+        D: "1850"
+    },
+    {
+        question: "What treaty ended the French and Indian War (1754-1763)?",
+        A: "Treaty of Versailles",
+        B: "Franco-Indian Treaty",
+        C: "Treaty of Alliance",
+        D: "Treaty of Paris"
+    },
+    {
+        question: "The undeclared war between the Unisted States and France fought on sea during John Adams's presidency was called the:",
+        A: "Franco-American Naval War",
+        B: "Atlantic Front War",
+        C: "Quasi War",
+        D: "American Anti-Alliance War"
+    },
+    {
+        question: "The formal abolishment of slavery was provided for with this important official document:",
+        A: "13th Amendment",
+        B: "Emancipation Proclamation",
+        C: "Freedom For Slaves Act",
+        D: "Civil War Treaty"
+    },
+    {
+        question: "In which court case was the desecration of the American flag prohibited in at least 48 of the 50 states?",
+        A: "Roe v. Wade",
+        B: "Texas v. Johnson",
+        C: "Citizens United v. Federal Election Commission",
+        D: "Doe v. The Patriots"
+    },
+    {
+        question: "The first American colony was settled in this city and state:",
+        A: "Jamestown, Virginia",
+        B: "Plymouth, Massachusetts",
+        C: "Yorktown, Virginia",
+        D: "Chesapeake, Virgina"
+    },
+    {
+        question: "In which battle was 'the shot heard round the world' fired?",
+        A: "Battle of the Highlands",
+        B: "Battle of Concord",
+        C: "Battle of Virginia Hills",
+        D: "The American Revolutionary Battle"
+    },
+    {
+        question: "In what year did the Balitmore & Ohio Railroad become America's first charter company",
+        A: "1851",
+        B: "1816",
+        C: "1803",
+        D: "1827"
+    },
+    {
+        question: "The international alliance NATO (North Atlantic Treaty Organization) currently consists of how many allies including the United States?",
+        A: "50",
+        B: "10",
+        C: "29",
+        D: "35"
+    },
+]
