afba7183e38114218f0da2ef081ecd98ce4eb0ce
Davey Wong <notwdjw@gmail.com>
2019-11-08 17:27:51 -0800

    Added a function that will display buttons and and event listener that tracks which button the user pushed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,70 +1,81 @@
 //For the trivia questions, we will want to use an array of objects that have a question and ABCD answers
 // This way we can loop through the array and display the question and all of the answers at once
 //The theme of questions will come from history
-
+var qn = 0;
+var wrong = 0;
+var right = 0;
 var qanda = [
     {
         question: "Which US president served the shortest term in office?",
         A: "William Henry Harrison",
         B: "James A. Garfield",
         C: "Zachary Taylor",
-        D: "Gerald Ford"
+        D: "Gerald Ford",
+        answer: "A"
     },
     {
         question: "In what year was the Louisiana Purchase made?",
         A: "1784",
         B: "1909",
         C: "1803",
-        D: "1850"
+        D: "1850",
+        answer: "C"
     },
     {
         question: "What treaty ended the French and Indian War (1754-1763)?",
         A: "Treaty of Versailles",
         B: "Franco-Indian Treaty",
         C: "Treaty of Alliance",
-        D: "Treaty of Paris"
+        D: "Treaty of Paris",
+        answer: "D"
     },
     {
         question: "The undeclared war between the Unisted States and France fought on sea during John Adams's presidency was called the:",
         A: "Franco-American Naval War",
         B: "Atlantic Front War",
         C: "Quasi War",
-        D: "American Anti-Alliance War"
+        D: "American Anti-Alliance War",
+        answer: "C"
     },
     {
         question: "The formal abolishment of slavery was provided for with this important official document:",
         A: "13th Amendment",
         B: "Emancipation Proclamation",
         C: "Freedom For Slaves Act",
-        D: "Civil War Treaty"
+        D: "Civil War Treaty",
+        answer: "A"
     },
     {
         question: "In which court case was the desecration of the American flag prohibited in at least 48 of the 50 states?",
         A: "Roe v. Wade",
         B: "Texas v. Johnson",
         C: "Citizens United v. Federal Election Commission",
-        D: "Doe v. The Patriots"
+        D: "Doe v. The Patriots",
+        answer: "B"
     },
     {
         question: "The first American colony was settled in this city and state:",
         A: "Jamestown, Virginia",
         B: "Plymouth, Massachusetts",
         C: "Yorktown, Virginia",
-        D: "Chesapeake, Virgina"
+        D: "Chesapeake, Virgina",
+        answer: "A"
     },
     {
         question: "In which battle was 'the shot heard round the world' fired?",
         A: "Battle of the Highlands",
         B: "Battle of Concord",
         C: "Battle of Virginia Hills",
-        D: "The American Revolutionary Battle"
+        D: "The American Revolutionary Battle",
+        answer: "B"
     },
     {
         question: "In what year did the Balitmore & Ohio Railroad become America's first charter company",
         A: "1851",
         B: "1816",
         C: "1803",
-        D: "1827"
+        D: "1827",
+        answer: "D"
     },
     {
         question: "The international alliance NATO (North Atlantic Treaty Organization) currently consists of how many allies including the United States?",
@@ -74,3 +85,44 @@ var qanda = [
         D: "35"
     },
 ]
+
+// Next we will want a function that displays the the question and answers onto our page
+function displayToPage(number) {
+    qn = number;
+    $('#question').html("Question " + (number + 1) + ": " + qanda[number].question)
+    $('#a').html("<button id='answer' class='A'>" + "A" + "</button>" + " " + qanda[number].A)
+    $('#b').html("<button id='answer' class='B'>" + "B" + "</button>" + " " + qanda[number].B)
+    $('#c').html("<button id='answer' class='C'>" + "C" + "</button>" + " " + qanda[number].C)
+    $('#d').html("<button id='answer' class='D'>" + "D" + "</button>" + " " + qanda[number].D)
+}
+// Make sure the document is ready, then we can click away!
+$(document).ready(function () {
+    //We will have an event listener that listens for clicks on the buttons and then checks the answer based on the button clicked 
+    $(".buttons").on("click", '#answer', function (event) {
+        event.preventDefault(); //make sure the page doesn't reload when a button is pushed
+        //Store the actual answer and the given answer as variables
+        var anslet = qanda[qn].answer;
+        var ans = $(this).attr('class');
+        // console.log(ans);
+        // console.log(anslet);
+        if (ans === anslet) {//If the variables are equal, then they answered correctly
+            alert("You got it right!");
+            qn++; //We will only use this if we're wrapping the whole trivia into a loop
+            right++;
+            // console.log(qn);
+        }
+        else if (ans !== anslet) {//If not, then they answered incorrectly
+            // console.log("clicked");
+
+            alert("You got it wrong! The answer was: " + qanda[qn][anslet]);
+            qn++; //We will only use this if we're wrapping the whole trivia into a loop
+            wrong++;
+            // console.log(qn);
+            //
+        }
+
+    })
+})
+
+
+displayToPage(0);
